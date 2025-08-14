c176fcc70f2a55131002e621df5736d38ffe173c
AbuBokorprog <abubokor1066@gmail.com>
2025-08-14 21:22:26 +0600

    feat: refactor layout and navigation components, remove unused files for cleaner codebase

--- src/app/(super-admin)/layout.tsx
+++ src/app/(super-admin)/layout.tsx
@@ -4,6 +4,6 @@ interface Props {
   readonly children: ReactNode;
 }
 
-export default function UserLayout({ children }: Props) {
+export default function SuperAdminLayout({ children }: Props) {
   return <main className="container mx-auto px-10">{children}</main>;
 }
