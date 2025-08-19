edba5f4df15aaff872fe42cab2575da664b7dd06
anasahammad <anasahammad2002@gmail.com>
2025-08-19 23:24:39 +0600

    Refactor FilterCategoryButtons component for improved readability and structure

--- src/components/pages/categories/FilterCategoryButtons.tsx
+++ src/components/pages/categories/FilterCategoryButtons.tsx
@@ -2,7 +2,9 @@
 
 import { useState } from "react"
 import { Button } from "../../ui/button"
+import { cn } from "@/src/lib/utils"
 
+const mainCategories = ["Actors", "Actress", "Identities", "Story Beats"]
 
 const typeCategories = [
   "All",
@@ -19,15 +21,49 @@ const typeCategories = [
 
 const sortOptions = ["Popular", "Latest"]
 
-const FilterCategoryButtons =()=> {
+const FilterCategoryButtons = () => {
+  const [activeCategory, setActiveCategory] = useState("Actors")
   const [activeType, setActiveType] = useState("All")
   const [activeSort, setActiveSort] = useState("Popular")
 
   return (
-    <div className="w-full  space-y-6">
+    <div className="w-full space-y-6">
+      {/* Main Categories Section */}
+      <div className="space-y-3">
+       
+        <div className="flex flex-wrap gap-2 border-b border-gray-100 pb-4">
+          {mainCategories.map((category) => (
+            <Button
+              key={category}
+              variant="outline"
+              size="sm"
+              active={activeCategory === category}
+              onClick={() => setActiveCategory(category)}
+              className={
+                cn("px-4 py-2 border-none hover:bg-transparent hover:text-primary-rose text-xl", 
+                  category === activeCategory ? "text-primary-rose" : "text-white"
+                )
+              }
+            >
+              {category}
+
+            
+            </Button>
+          ))}
+        </div>
+      </div>
+
       {/* Type Section */}
       <div className="space-y-3">
-        <h3 className="text-white text-sm font-medium">Type</h3>
+        <h3 className="text-white text-sm font-medium">
+          {activeCategory === "Actors"
+            ? "Actor Types"
+            : activeCategory === "Actress"
+              ? "Actress Types"
+              : activeCategory === "Identities"
+                ? "Identity Types"
+                : "Story Beat Types"}
+        </h3>
         <div className="flex flex-wrap gap-2">
           {typeCategories.map((category) => (
             <Button
