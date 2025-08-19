793263a7b68b7b0a3eb8ae56068bd072f397d522
AbuBokorprog <abubokor1066@gmail.com>
2025-08-20 01:29:45 +0600

    Fixed: Error.

--- src/components/pages/episodes/DetailsCard.tsx
+++ src/components/pages/episodes/DetailsCard.tsx
@@ -1,38 +1,38 @@
-"use client"
+"use client";
 import { ChevronDown, ChevronRight, Eye, Forward, SquarePlay, ThumbsUp } from "lucide-react";
 import Image from "next/image";
-import { Button } from "../ui/button";
 import { useState } from "react";
 import ThumbnailCard from "./ThumbnailCard";
 
+const DetailsCard = ({ description, genres }: { description: string; genres: string[] }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-const DetailsCard = ({description, genres}:{description:string, genres:string[]}) => {
-    const [isExpanded, setIsExpanded] = useState(false);
+  const shouldShowSeeMore = description.length > 100;
+  const displayText =
+    shouldShowSeeMore && !isExpanded ? description.slice(0, 200) + "..." : description;
 
-    const shouldShowSeeMore = description.length > 100;
-    const displayText = shouldShowSeeMore && !isExpanded ? description.slice(0, 200) + "..." : description;
+  return (
+    <div className="mt-8">
+      <ThumbnailCard genres={genres} isShowRightIcon />
+      {/* Description */}
+      <div className="mt-6 w-full rounded-2xl bg-[#16151A] p-4">
+        <h3 className="mb-2 text-xl font-semibold text-white">Description</h3>
+        <p className="text-sm text-[#B3B1B0]">{displayText}</p>
 
-    return (
-        <div className="mt-8">
-
-              <ThumbnailCard genres={genres} isShowRightIcon/>
-              {/* Description */}
-              <div className="bg-[#16151A] w-full p-4 rounded-2xl mt-6">
-                <h3 className="text-white text-xl font-semibold mb-2">Description</h3>
-                <p className="text-sm text-[#B3B1B0] ">{displayText}</p>
-
-                 {shouldShowSeeMore && (
+        {shouldShowSeeMore && (
           <button
             onClick={() => setIsExpanded(!isExpanded)}
-            className="flex items-center gap-1 text-white text-sm mt-2 hover:text-blue-300 transition-colors cursor-pointer"
+            className="mt-2 flex cursor-pointer items-center gap-1 text-sm text-white transition-colors hover:text-blue-300"
           >
             <span>{isExpanded ? "See less" : "See more"}</span>
-            <ChevronDown className={`w-4 h-4 transition-transform duration-200 ${isExpanded ? "rotate-180" : ""}`} />
+            <ChevronDown
+              className={`h-4 w-4 transition-transform duration-200 ${isExpanded ? "rotate-180" : ""}`}
+            />
           </button>
         )}
-              </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DetailsCard;
