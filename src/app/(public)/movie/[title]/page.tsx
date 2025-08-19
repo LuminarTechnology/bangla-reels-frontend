5a04806d4dada0d3a8f748b34e8be36724b02563
AbuBokorprog <abubokor1066@gmail.com>
2025-08-19 15:11:39 +0600

    created reusable breadcrumb.

--- src/app/(public)/movie/[title]/page.tsx
+++ src/app/(public)/movie/[title]/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import YouMayLike from "@/src/components/pages/movie/YouMayLike";
 import MovieDetailsMainSection from "@/src/components/pages/movie/MovieDetailsMainSection";
+import ReusableBreadcrumb from "@/src/components/common/ReusableBreadcrumb";
 
 const MoviesDetailsPage = () => {
   return (
     <div className="min-h-screen">
       {/* Navigation */}
+
       <nav className="p-6">
-        <div className="text-sm text-white/70">Home / John Wick 4</div>
+        <ReusableBreadcrumb items={[{ label: "Home", href: "/" }, { label: "John Wick 4" }]} />
       </nav>
 
       {/* Main Content */}
---
+++ src/components/common/ReusableBreadcrumb.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "../ui/breadcrumb";
+
+interface BreadcrumbItemType {
+  label: string;
+  href?: string;
+}
+
+interface ReusableBreadcrumbProps {
+  items: BreadcrumbItemType[];
+}
+
+const ReusableBreadcrumb: React.FC<ReusableBreadcrumbProps> = ({ items }) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {items.map((item, idx) => (
+          <React.Fragment key={idx}>
+            <BreadcrumbItem>
+              {item.href ? (
+                <BreadcrumbLink
+                  className="text-sm text-white/70 hover:text-red-700"
+                  href={item.href}
+                >
+                  {item.label}
+                </BreadcrumbLink>
+              ) : (
+                <BreadcrumbPage className="text-sm text-white/70 hover:text-red-700">
+                  {item.label}
+                </BreadcrumbPage>
+              )}
+            </BreadcrumbItem>
+            {idx < items.length - 1 && <BreadcrumbSeparator />}
+          </React.Fragment>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default ReusableBreadcrumb;
