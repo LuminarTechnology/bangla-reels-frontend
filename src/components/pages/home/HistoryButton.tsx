c84dbaa7bef786cc931eca1e67d306c7f049f24f
anasahammad <anasahammad2002@gmail.com>
2025-08-19 21:49:45 +0600

    Refactor Navbar to use HistoryButton component; create HistoryContent for displaying history and my list items

--- src/components/pages/home/HistoryButton.tsx
+++ src/components/pages/home/HistoryButton.tsx
@@ -1,10 +1,110 @@
+import { History } from "lucide-react";
+import { Button } from "../../ui/button";
 
+import Image from "next/image";
+import Link from "next/link";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "../../ui/hover-card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../ui/tabs";
+import HistoryContent from "./HistoryContent";
+import { useRouter } from "next/navigation";
 
 const HistoryButton = () => {
+  const router = useRouter()
+      const historyItems = [
+    {
+      id: 1,
+      title: "Collateral Hearts to gone",
+      episode: 1,
+      thumbnail: "/images/Poster 4.png",
+    },
+    {
+      id: 2,
+      title: "Collateral Hearts to gone",
+      episode: 1,
+      thumbnail: "/images/Poster 4.png",
+    },
+    {
+      id: 3,
+      title: "Collateral Hearts to gone",
+      episode: 1,
+      thumbnail: "/images/Poster 4.png",
+    },
+  ]
+
+  const myListItems = [
+    {
+      id: 1,
+      title: "My List Item 1",
+      episode: 1,
+      thumbnail: "/images/Poster 4.png",
+    },
+    {
+      id: 2,
+      title: "My List Item 2",
+      episode: 2,
+      thumbnail: "/images/Poster 4.png",
+    },
+    {
+      id: 3,
+      title: "My List Item 3",
+      episode: 3,
+      thumbnail: "/images/Poster 4.png",
+    },
+    {
+      id: 4,
+      title: "My List Item 2",
+      episode: 2,
+      thumbnail: "/images/Poster 4.png",
+    },
+    {
+      id: 5,
+      title: "My List Item 3",
+      episode: 3,
+      thumbnail: "/images/Poster 4.png",
+    },
+  ]
     return (
-        <div>
-            
-        </div>
+         <HoverCard openDelay={100} closeDelay={300}>
+                <HoverCardTrigger asChild>
+                   <Link href={"/dashboard/history"}>
+              <Button
+                variant="ghost"
+                className="hidden text-white hover:bg-transparent hover:text-gray-100 lg:flex"
+              >
+                <History className="h-4 w-4" />
+                History
+              </Button>
+            </Link>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-70 bg-[#16151A] border-none p-0 rounded-2xl " align="end" sideOffset={8}>
+                  <div className="p-4">
+                  
+                    <Tabs defaultValue="history" className="w-full">
+                      <TabsList className="grid w-full grid-cols-2 bg-transparent border-none mb-4 ">
+                        <TabsTrigger
+                          value="history"
+                          className="text-white data-[state=active]:bg-transparent  border-0 !border-none ring-0 focus:ring-0 data-[state=active]:text-[#E83A57] hover:text-primary-rose-hover cursor-pointer"
+                        >
+                          History
+                        </TabsTrigger>
+                        <TabsTrigger
+                          value="mylist"
+                          className="text-white data-[state=active]:bg-transparent  border-0 !border-none ring-0 focus:ring-0 data-[state=active]:text-[#E83A57] hover:text-primary-rose-hover cursor-pointer"
+                        >
+                          My list
+                        </TabsTrigger>
+                      </TabsList>
+                      <TabsContent value="history" className="space-y-3">
+                       <HistoryContent historyItems={historyItems} tabName="History" link="/dashboard/history"/>
+                      </TabsContent>
+                      <TabsContent value="mylist" className="space-y-3">
+
+                       <HistoryContent historyItems={myListItems} tabName="My List" link="/dashboard/my-list"/>
+                      </TabsContent>
+                    </Tabs>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
     );
 };
 
---
+++ src/components/pages/home/HistoryContent.tsx
@@ -0,0 +1,58 @@
+import { ChevronRight, PlayCircle } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+interface HistoryContentProps {
+    historyItems: {
+        id: number,
+        title: string,
+        episode: number,
+        thumbnail: string
+    }[],
+    tabName: string,
+    link:string
+}
+const HistoryContent = ({historyItems, tabName, link}: HistoryContentProps) => {
+    return (
+        <>
+             {historyItems.length === 0 ? 
+           (  
+            <div className="flex flex-col items-center space-y-3">
+                          <PlayCircle className="h-12 w-12 text-[#B3B1B0]" />
+                          <p className="text-[#B3B1B0] text-sm">You haven't watched any videos yet.</p>
+                        </div>
+           )
+             : historyItems.slice(0, 3).map((item) => (
+                          <div
+                            key={item.id}
+                            className="flex  space-x-3 p-2 rounded-lg hover:bg-gray-900 cursor-pointer"
+                          >
+                            <div className="relative">
+                              <Image
+                              src={item.thumbnail || "/placeholder.svg"}
+                              alt={item.title}
+                              width={69}
+                              height={93}
+                              className="w-[69px] h-[93px] rounded object-cover"
+                            />
+                            </div>
+                            <div className="flex-1 min-w-0">
+                              <h3 className="text-white text-xl font-bold ">{item.title}</h3>
+                              <p className="text-[#B3B1B0] text-base">Played to Episode {item.episode}</p>
+                            </div>
+                          </div>
+                        ))}
+                       {historyItems.length >0 && (
+                         <div className="pt-2 flex justify-center">
+                          <Link href={link} className="text-primary-rose text-sm font-medium hover:text-primary-rose-hover flex items-center">
+                            More
+                            <ChevronRight className="w-4 h-4 ml-1" />
+                          </Link>
+                        </div>
+                       )}
+        </>
+    );
+};
+
+export default HistoryContent;
