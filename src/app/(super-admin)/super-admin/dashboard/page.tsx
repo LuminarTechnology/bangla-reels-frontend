4aa7d0825e8284e09b780bc8676670b0e02903c3
anasahammad <anasahammad2002@gmail.com>
2025-08-21 21:53:31 +0600

    Add DashboardMetrics and MetricCard components to SuperAdminDashboard

--- src/app/(super-admin)/super-admin/dashboard/page.tsx
+++ src/app/(super-admin)/super-admin/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { DashboardHeader } from "@/src/components/pages/super-admin/DashboardHeader";
+import DashboardMetrics from "@/src/components/pages/super-admin/DashboardMetrics";
 import React from "react";
 
 export default function SuperAdminDashboard() {
   return (
     <div>
       <DashboardHeader/>
+      <DashboardMetrics/>
     </div>
   );
 }
--- src/components/pages/super-admin/DashboardHeader.tsx
+++ src/components/pages/super-admin/DashboardHeader.tsx
@@ -8,10 +8,10 @@ import { Popover, PopoverContent, PopoverTrigger } from "../../ui/popover";
 export function DashboardHeader() {
   const today = new Date();
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between border-b border-[#E0E0E0] mb-4 pb-4">
       <div className="text-[#242424]">
         <h1 className="text-2xl font-semibold">Hi Saiful</h1>
-        <p className="text-sm">Welcome to RentBird</p>
+        <p className="text-sm">Welcome to Reelshort</p>
       </div>
       <Popover>
         <PopoverTrigger asChild>
