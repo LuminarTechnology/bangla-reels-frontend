27075324823d2a63ed4989b40aa7a81e3f27f3f9
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-08-27 20:46:56 +0600

    feat: add super admin order history page UI with tabs and tables
    Implemented the UI for the super admin order history page, featuring:
    - Tabs for navigating between coin and VIP plan order histories
    - Content tables in each tab to display respective order history data
    - Responsive and clean design for improved usability

---
+++ src/app/(super-admin)/super-admin/dashboard/order-history/page.tsx
@@ -0,0 +1,15 @@
+import CoinHistoryTable from "@/src/components/pages/super-admin/order-history/CoinHistoryTable"
+import OrderHistoryTabs from "@/src/components/pages/super-admin/order-history/OrderHistoryTabs"
+
+const Page = () => {
+  return (
+    <>
+        <div className="bg-[#F3F4F6] rounded-2xl p-2 md:px-9 md:py-6">
+            <h3 className="text-2xl font-semibold mb-4">Order History</h3>
+            <OrderHistoryTabs/>
+        </div>
+    </>
+  )
+}
+
+export default Page
---
+++ src/components/pages/super-admin/order-history/OrderHistoryTabs.tsx
@@ -0,0 +1,40 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/src/components/ui/tabs";
+import CoinHistoryTable from "./CoinHistoryTable";
+import VIPHistoryTable from "./VIPHistoryTable";
+import { Separator } from "@/src/components/ui/separator";
+
+const tabs = [
+  {
+    name: "Coin Plan",
+    value: "coin-plan",
+    content: <CoinHistoryTable />,
+  },
+  {
+    name: "VIP Plan",
+    value: "vip plan",
+    content: <VIPHistoryTable />,
+  },
+];
+export default function TabsWithBadgeDemo() {
+  return (
+    <Tabs defaultValue={tabs[0].value} className="w-full relative">
+      <Separator className="absolute top-9" />
+      <TabsList className="w-fit justify-start gap-1 rounded-none border-b p-0">
+        {tabs.map((tab) => (
+          <TabsTrigger
+            key={tab.value}
+            value={tab.value}
+            className="data-[state=active]:border-b-primary h-full rounded-none border-b-2 border-transparent pb-4 pl-0 text-[#4E4E4E] data-[state=active]:bg-[#F3F4F6] data-[state=active]:text-black data-[state=active]:shadow-none"
+          >
+            <span className="text-base">{tab.name}</span>
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {tabs.map((tab) => (
+        <TabsContent key={tab.value} value={tab.value}>
+          {tab.content}
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+}
