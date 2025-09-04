ce33df13d30ce66d0e6174f1de064af13247c4cc
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-09-04 22:58:38 +0600

    fix: unexpected scrollbar issue fixed.

--- src/app/(super-admin)/[lang]/super-admin/dashboard/layout.tsx
+++ src/app/(super-admin)/[lang]/super-admin/dashboard/layout.tsx
@@ -16,7 +16,7 @@ export default async function SuperAdminDashboardLayout({ children }: Props) {
   //   redirect("/sign-in");
   // }
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen max-h-screen bg-gray-50">
       {/* Sidebar */}
       <DashboardSidebar sidebarItems={SuperAdminSidebarItems} />
 
