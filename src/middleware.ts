aea65bd5f0c062bbff773dc67b2d8b8e2ed226a8
AbuBokorprog <abubokor1066@gmail.com>
2025-09-05 09:21:13 +0600

    clerk and next-intl middleware comment out to speed up website (Experimental)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,56 +1,60 @@
 import { NextRequest, NextResponse, NextFetchEvent } from "next/server";
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import createIntlMiddleware from "next-intl/middleware";
-import { Roles } from "./types/globals";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import createIntlMiddleware from "next-intl/middleware";
+// import { Roles } from "./types/globals";
 
-const LOCALES = ["en", "bn", "es", "la"];
+// const LOCALES = ["en", "bn", "es", "la"];
 const DEFAULT_LOCALE = "en";
 
-const intlMiddleware = createIntlMiddleware({
-  locales: LOCALES,
-  defaultLocale: DEFAULT_LOCALE,
-  localeDetection: true,
-});
+// const intlMiddleware = createIntlMiddleware({
+//   locales: LOCALES,
+//   defaultLocale: DEFAULT_LOCALE,
+//   localeDetection: true,
+// });
 
-const isAdminRoute = createRouteMatcher(["/:locale/super-admin(.*)"]);
-const isTopUpRoute = createRouteMatcher(["/:locale/dashboard/top-up"]);
+// const isAdminRoute = createRouteMatcher(["/:locale/super-admin(.*)"]);
+// const isTopUpRoute = createRouteMatcher(["/:locale/dashboard/top-up"]);
 
 //  Auth Middleware
-const authHandler = clerkMiddleware(async (auth, req: NextRequest) => {
-  const { pathname } = req.nextUrl;
-  const { sessionClaims } = await auth();
-  const lang = req.cookies.get("locale")?.value || DEFAULT_LOCALE;
+// const authHandler = clerkMiddleware(async (auth, req: NextRequest) => {
 
-  const roles: Roles[] = (sessionClaims?.metadata?.roles as Roles[]) || [];
+//   const { sessionClaims } = await auth();
+//   const lang = req.cookies.get("locale")?.value || DEFAULT_LOCALE;
 
-  if (pathname === "/") {
-    return NextResponse.redirect(new URL(`/${lang}`, req.url));
-  }
+//   const roles: Roles[] = (sessionClaims?.metadata?.roles as Roles[]) || [];
 
-  if (process.env.NODE_ENV !== "development") {
-    // Admin route protection
-    if (isAdminRoute(req) && !roles.includes("superAdmin")) {
-      const url = new URL(`/${lang}/sign-in`, req.url);
-      return NextResponse.redirect(url);
-    }
+//   if (process.env.NODE_ENV !== "development") {
+//     // Admin route protection
+//     if (isAdminRoute(req) && !roles.includes("superAdmin")) {
+//       const url = new URL(`/${lang}/sign-in`, req.url);
+//       return NextResponse.redirect(url);
+//     }
 
-    if (isTopUpRoute(req) && roles.length === 0) {
-      return NextResponse.redirect(new URL(`/${lang}/sign-in`, req.url));
-    }
-  }
+//     if (isTopUpRoute(req) && roles.length === 0) {
+//       return NextResponse.redirect(new URL(`/${lang}/sign-in`, req.url));
+//     }
+//   }
 
-  return NextResponse.next();
-});
+//   return NextResponse.next();
+// });
 
 //  Compose Middleware
 export async function middleware(req: NextRequest, event: NextFetchEvent) {
   //Locale handling
-  const intlResponse = intlMiddleware(req);
-  if (intlResponse) return intlResponse;
+  // const intlResponse = intlMiddleware(req);
+  // if (intlResponse) return intlResponse;
 
   // Auth + Role check
-  const authResponse = await authHandler(req, event);
-  if (authResponse) return authResponse;
+  // const authResponse = await authHandler(req, event);
+  // if (authResponse) return authResponse;
+
+  // return NextResponse.next();
+
+  const { pathname } = req.nextUrl;
+  const lang = req.cookies.get("locale")?.value || DEFAULT_LOCALE;
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL(`/${lang}`, req.url));
+  }
 
   return NextResponse.next();
 }
