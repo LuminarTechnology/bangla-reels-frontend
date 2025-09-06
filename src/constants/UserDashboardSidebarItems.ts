3fd31c9586b4c5dd5620bc88fb47eb0cbbdac313
anasahammad <anasahammad2002@gmail.com>
2025-09-06 19:26:00 +0600

    refactor UserDashboardSidebarItems to update Analytics icon and improve import formatting

--- src/constants/UserDashboardSidebarItems.ts
+++ src/constants/UserDashboardSidebarItems.ts
@@ -1,10 +1,17 @@
-import {  AwardIcon, LaptopMinimal, SquarePlay, MailOpen, ClockFading  } from "lucide-react";
+import {
+  AwardIcon,
+  LaptopMinimal,
+  SquarePlay,
+  MailOpen,
+  ClockFading,
+  ChartNoAxesCombined,
+} from "lucide-react";
 
 export const UserDashboardSidebarItems = [
   { name: "Overview", href: "/dashboard", icon: LaptopMinimal },
   { name: "Subscription & Rewards", href: "/dashboard/subscription-rewards", icon: AwardIcon },
   { name: "My List", href: "/dashboard/my-list", icon: SquarePlay },
   { name: "History", href: "/dashboard/history", icon: ClockFading },
-  { name: "Analytics", href: "/dashboard/analytics", icon: ClockFading },
+  { name: "Analytics", href: "/dashboard/analytics", icon: ChartNoAxesCombined },
   { name: "Feedback", href: "/dashboard/feedback", icon: MailOpen },
 ];
